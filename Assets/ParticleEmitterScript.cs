1df23d0f2ce159c7fc070ba164b7c0b4f2af873c
Sebastian Thimmig <thimmig.2@osu.edu>
2013-09-28 14:40:25 -0400

    Scene with plane, particle emitter, particle prefab, & ParticleScript that impliments basic position updates

---
+++ Assets/ParticleEmitterScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleEmitterScript : MonoBehaviour {
+
+	public Transform myParticle;
+	
+	private List<GameObject> particles;
+	private int particleCount = 40;
+
+	// Use this for initialization
+	void Start () {
+		particles = new List<GameObject>();
+		
+		for(int i=0; i<particleCount; i++) {
+			particles.Add(createParticle());			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// remove all particles that have died
+		particles.RemoveAll(item => item == null);
+		
+		// respawn new particles
+		for(int i = particles.Count; i < particleCount; i++) {
+			particles.Add(createParticle());
+		}
+	}
+
+	private GameObject createParticle() {
+		Component newParticle = (Component)Instantiate(myParticle, transform.position, transform.rotation);
+		newParticle.GetComponent<ParticleScript>().setup(transform);
+		return newParticle.gameObject;
+	}
+}
---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+
+	private static int count = 0;
+	private static float timeStep = Time.deltaTime * 40;
+	private static float massMin = 1.0F,
+							massMax = 10.0F;
+	
+	private static float vXMin = -10.0F,
+							vXMax = 10.0F,
+							vYMin = -10.0F,
+							vYMax = 10.0F,
+							vZMin = -10.0F,
+							vZMax = 10.0F;
+
+	private static float fXMin = -1000.0F,
+							fXMax = 1000.0F,
+							fYMin = -1000.0F,
+							fYMax = 1000.0F,
+							fZMin = -1000.0F,
+							fZMax = 1000.0F;
+
+	private Vector3 position, velocity, acceleration, force;
+	private int age, maxAge;
+	private float mass;
+
+
+	public void setup(Transform parent) {
+		transform.name = "Particle" + count;
+		ParticleScript.count++;
+
+		transform.parent = parent;
+		this.age = 0;
+		this.maxAge = 1000;
+		this.mass = Random.Range(massMin, massMax);
+		this.velocity = new Vector3(Random.Range(vXMin, vXMax), Random.Range(vYMin, vYMax), Random.Range(vZMin, vZMax));
+		this.force = new Vector3(Random.Range(fXMin, fXMax), Random.Range(fYMin, fYMax), Random.Range(fZMin, fZMax));
+	}
+
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		ageCheck();
+
+		updateAcceleration();
+		updateVelocity();
+		updatePosition();
+		age++;
+	}
+
+	private void updatePosition() {
+		transform.position += this.velocity * ParticleScript.timeStep;
+	}
+
+	private void updateVelocity() {
+		this.velocity += this.acceleration * ParticleScript.timeStep;
+	}
+
+	private void updateAcceleration() {
+		this.acceleration = this.force/this.mass;
+	}
+
+	private void ageCheck() {
+		if(this.age >= this.maxAge) {
+			ParticleScript.count--;
+			Destroy(transform.gameObject);
+		}
+	}
+
+
+
+}
